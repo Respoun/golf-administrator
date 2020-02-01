366397613e456a29b4a757313cf3a7f2398d3901
Guillaume FITAMANT <Guillaume.FITAMANT@cloud-temple.com>
2020-02-01 11:40:00 +0100

    Add serveur + Db connection

---
+++ src/configs/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb://guigz:jesuisunelicorne1@ds215219.mlab.com:15219/golf-administrator"
+}
---
+++ src/services/express.service.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const config = require('../configs/server.config');
+const bodyParser = require('body-parser');
+// const apiRouter = require('../routes');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+//routes
+// app.use('/api/v1', apiRouter);
+
+exports.start = () => {
+    let port = config.port;
+
+    app.listen(port, (err) => {
+        if (err) {
+            console.log(`Error:${err}`);
+            process.exit(-1);
+        }
+        console.log(`app is running on port ${port}`);
+    });
+
+}
